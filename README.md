651bac3548063cc27a7b3f364bed1c0e52eed00f
charyxiao <charyxiao@qq.com>
2018-01-25 15:59:58 +0000

    init myworkspace with on shell script

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# MVim
-## For YouCompleteMe You will need to do:
+# MyWorkSpace 
+
+## Installation
+* git clone https://github.com/charyxiao/MyWorkSpace.git
+* cd MyWorkSpace && sh install.sh
+
+## required
+* git vim8 tmux ctags cscope
+* if you are using centos7, see install.sh line2-6
+
+## For YouCompleteMe You will need to do [default without YCM]
 * For Centos: python python-devel gcc gcc-c++ cmake make
 * For Ubuntu: python python-dev build-essential
 * python install.py --clang-completer
+
 ## Font
 * Source Code Pro is a nice font! 
+* Courier New is also a nice font! 
--- install.sh
+++ install.sh
@@ -1,35 +1,70 @@
 #!/bin/bash
+# curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+# yum update vim*
+# yum install git
+# yum install tmux
+# yum isntall ctage cscope
 
-echo "It will install some vim plugins in you computer, will overwrite your .vimrc." \
-      "\nFor YouCompleteME insure you have installed cmake and python2.7 or python3.0"
-echo -e "Continue Or Quit(y/n):\c"
-read discision
-if [ "$discision" != "y" -a "$discision" != "yes" ]
-then
-    echo "GoodBye!"
-    exit 0
-fi
+echo "Step 1: set up ps1 && git [Be Sure git has been Installed!]"
+
+cp git-completion.bash ~/.git-completion.bash
+cp bash_git_ps1.sh ~/.bash_git_ps1.sh
+cp myalias ~/.myalias.sh
+grep -qE .git-completion.bash\|.bash_git_ps1.sh ~/.bashrc || cat >> ~/.bashrc << '__EOF__'
+test -f ~/.git-completion.bash && source ~/.git-completion.bash
+test -f ~/.bash_git_ps1.sh && source ~/.bash_git_ps1.sh
+test -f ~/.myalias.sh && source ~/.myalias.sh
+__EOF__
+
+# set gpush
+#git config --global alias.gpush '!f() { : push ; r=$1; [[ -z $r ]] && r=origin; b=$2; t=$(awk "{ print \$2 }" $(git rev-parse --git-dir)/HEAD); t=${t#refs/heads/}; [[ -z $b ]] && b=$t; cmd="git push $r HEAD:refs/for/$b%topic=$t"; echo $cmd; echo; $cmd; }; f'
+
+# set color
+git config --global user.name "charyxiao"
+git config --global user.email charyxiao@qq.com
+git config --global color.ui auto
+
+# set log alias
+git config --global alias.lg 'log --stat'
+git config --global alias.lgp 'log --stat -p'
+git config --global alias.lgg 'log --graph'
+git config --global alias.lgga 'log --graph --decorate --all'
+git config --global alias.lgm 'log --graph --max-count=10'
+git config --global alias.lo 'log --oneline --decorate'
+git config --global alias.lol "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lola "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+git config --global alias.log 'log --oneline --decorate --graph'
+git config --global alias.loga 'log --oneline --decorate --graph --all'
 
 
-echo "Step 1: cp vimrc vimrc.bundles -->.vimrc .vimrc.bundles "
+echo "Step 2: set up vim[Be Sure Vim8/ctags/ccscope has been Installed]"
+with_ycm=0
+
 cp vimrc "${HOME}/.vimrc"
-cp vimrc.bundles "${HOME}/.vimrc.bundles"
-mkdir -p "${HOME}/.vim/bundle/"
-git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/vundle
-
-echo "Step 2: Update/Install plugins using Plugin"
-shell_back=$SHELL
-export SHELL="/bin/sh"
-vim -u ${HOME}/.vimrc.bundles +PlugInstall! +PlugClean! +qall
-export SHELL=$shell_back
-
-echo "Step 4: Compile YouCompleteMe"
-cd ${HOME}/.vim/bundle/YouCOmpleteMe
-if [ `which clang` ]
-then
-    python install.py --clang-completer --system-libclang
+if [[ $with_ycm -eq 1 ]]; then
+    cp vimrc.bundles.ycm "${HOME}/.vimrc.bundles"
 else
-    python install.py --clang-completer
+    cp vimrc.bundles "${HOME}/.vimrc.bundles"
+fi
+mkdir -p "${HOME}/.vim/bundle/"
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/vundle >&/dev/null
+
+vim -u ${HOME}/.vimrc.bundles +PlugInstall! +PlugClean! +qall
+
+if [[ $with_ycm -eq 1 ]]; then
+    echo "Compile YouCompleteMe...."
+    cd ${HOME}/.vim/bundle/YouCOmpleteMe
+    if [ `which clang` ]
+    then
+        python install.py --clang-completer --system-libclang
+    else
+        python install.py --clang-completer
+    fi
 fi
 
-echo "Install Done!"
+echo "Step 2: set up tmux[Be Sure tmux has been Installed]"
+
+cp tmux.conf "${HOME}/.tmux.conf"
+cp -r kits "${HOME}/"
+
+echo "Install Done! Please Relogin!"
