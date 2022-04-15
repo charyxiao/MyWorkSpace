9d92c5fbb94af253e2604cf59ab98729442ab141
dongxiao <dongxiao@tencent.com>
2022-04-15 17:36:18 +0800

    fix: 修复不存在问题source的问题

--- vim/install.sh
+++ vim/install.sh
@@ -3,13 +3,16 @@
 set -x -e
 
 # softs
-# ipath=/root
-# sh -x softs/install_libffi.sh $ipath
-# sh -x softs/install_python.sh $ipath && source ~/.bashrc
-# sh -x softs/install_vim.sh  $ipath
-# sh -x softs/install_universal_ctags.sh $ipath
-# sh -x softs/install_clang-format.sh $ipath
-# sh -x softs/install_node.sh $ipath
+ipath=/root
+sh -x softs/install_libffi.sh $ipath
+sh -x softs/install_python.sh $ipath && source ~/.bashrc
+echo "/root/python3.9/lib/" > /etc/ld.so.conf.d/python3.conf
+ldconfig
+sh -x softs/install_vim.sh  $ipath
+sh -x softs/install_universal_ctags.sh $ipath
+sh -x softs/install_clang-format.sh $ipath
+sh -x softs/install_node.sh $ipath
 
 # vim config
 sh -x config/install_vim_config.sh
+
